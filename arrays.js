45e606e842f1dc171619cb063d61924ae3f31057
Vira <virko1996@gmail.com>
2024-10-07 22:10:50 +0300

    added comments to the tasks in arrays.js

--- arrays.js
+++ arrays.js
@@ -6,11 +6,11 @@ console.log(names[names.length - 1]); // "Vira"
 
 // ! 2. Видаліть перший елемент масиву і виведіть його.
 const firstElement = names.shift();
-console.log(firstElement);
+console.log(firstElement); // "Anna"
 
 
 // ! 3. Знайдіть індекс елемента зі значенням "John" в масиві ["Mike", "John", "Sara"].
 const names2 = ['Mike', 'John', 'Sara'];
 const index = names2.indexOf('John');
-console.log(index);
+console.log(index); // 1
 
