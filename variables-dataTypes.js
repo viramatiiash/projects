04f856dff786641f15a652bc23a35026328d0c0a
Vira <virko1996@gmail.com>
2024-10-07 20:37:46 +0300

    first commit

---
+++ variables-dataTypes.js
@@ -0,0 +1,23 @@
+// ! 1. Створіть змінну, яка зберігає ім'я користувача. Виведіть значення цієї змінної в консоль.
+const name = "Vira";
+console.log(name); // "Vira" - string
+
+
+// ! 2. Створіть змінну, яка зберігає вік користувача. Перетворіть цю змінну на рядок і виведіть тип цієї змінної в консоль.
+const userAge = 27;
+const ageString = userAge.toString();
+console.log(ageString); // "27" - string
+
+
+// ! 3. Створіть змінну, яка зберігає число "10" і додайте до нього рядок "20". Виведіть результат і його тип.
+const number = 10;
+const add = number + "20";
+console.log(add); // "1020" - string
+
+
+
+// const add1 = '20' + number; 
+// console.log(add1); // ? "2010" - string
+
+
+
