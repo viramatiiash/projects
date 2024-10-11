aca052d87e59a92e861c8225a824906b2e149618
Vira <virko1996@gmail.com>
2024-10-11 13:48:49 +0300

    completed one more task about classes in codewars_8kyu.js

--- codewars_8kyu.js
+++ codewars_8kyu.js
@@ -94,4 +94,22 @@ function mergeArrays(arr1, arr2) {
   return uniqueArr;
 }
 
+// ! 11. Fun with ES6 Classes #1 - People, people, people - https://www.codewars.com/kata/56f7f8215d7c12c0e7000b19
+class Person {
+  constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
+
 
